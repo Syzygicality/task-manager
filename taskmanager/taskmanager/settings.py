152c804fbbaed934ad9142f5379afc861e99374d
Edison Wang <ew040607@gmail.com>
2025-08-22 21:39:13 +0000

    settings config to use MySQL

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = "taskmanager.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "mysql.connector.django",
+        "NAME": "taskmanager",
+        "USER": "root",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
