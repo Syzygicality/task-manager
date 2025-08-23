d2b012e85d47ed5bcdb8ed179c9d5c30b39424f6
Edison Wang <ew040607@gmail.com>
2025-08-23 23:45:58 +0000

    validating completion date

--- taskmanager/taskmanagerAPI/serializers.py
+++ taskmanager/taskmanagerAPI/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
 from .models import Task
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['id', 'description', 'completed', 'completion_date', 'user']
         read_only_fields = ['id', 'completed', 'user']
+    
+    def validate_completion_date(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError("Completion date cannot be in the past.")
+        return value
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
